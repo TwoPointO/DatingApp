56c167ec27e70a02346d9572c71996226bd38c34
Marko Curic <mcuric1710@gmail.com>
2020-11-21 17:13:56 +0100

    End of section 7

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -7,6 +7,7 @@ namespace API.DTOs
         [Required(ErrorMessage = "Username is required")]
         public string Username { get; set; }
         [Required(ErrorMessage = "Password is required")]
+        [StringLength(16, MinimumLength = 4)]
         public string Password { get; set; }
     }
 }
