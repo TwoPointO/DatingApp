e8ee0aa9eb5e2bd1f4cf6438033f4d90c23a6a84
Marko Curic <mcuric1710@gmail.com>
2020-11-30 10:48:12 +0100

    End of section 10

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -19,23 +19,20 @@ namespace API.Controllers
             return "secret text";
         }
 
-        [Authorize]
         [HttpGet("not-found")]
         public ActionResult<AppUser> GetNotFound() {
             var thing = _context.Users.Find(-1);
             if(thing == null) return NotFound();
-            return Ok(thing);
+            return thing;
         }
         
-        [Authorize]
         [HttpGet("server-error")]
         public ActionResult<string> GetServerError() {
             var thing = _context.Users.Find(-1);
             var thingToReturn = thing.ToString();
-            return Ok(thingToReturn);
+            return thingToReturn;
         }
 
-        [Authorize]
         [HttpGet("bad-request")]
         public ActionResult<string> GetBadRequest() {
             return BadRequest("This was not a good request!");
--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -27,7 +27,7 @@ namespace API.Middleware
             }
             catch(Exception ex) {
                 _logger.LogError(ex, ex.Message);
-                context.Response.ContentType = "application.json";
+                context.Response.ContentType = "application/json";
                 context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
                 var response = _env.IsDevelopment()
