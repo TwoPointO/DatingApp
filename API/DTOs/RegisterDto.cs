c13019facfb03d97399b499fd4836bfa15bb70e2
Marko Curic <mcuric1710@gmail.com>
2020-11-30 22:03:32 +0100

    End of section 12

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,3 +1,4 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace API.DTOs
@@ -6,6 +7,17 @@ namespace API.DTOs
     {
         [Required(ErrorMessage = "Username is required")]
         public string Username { get; set; }
+        [Required]
+        public string KnownAs { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Country { get; set; }
+        
         [Required(ErrorMessage = "Password is required")]
         [StringLength(16, MinimumLength = 4)]
         public string Password { get; set; }
